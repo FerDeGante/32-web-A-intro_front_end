189fc7494a09d2eaeb591aa5188cfe8d828a9866
@FerDeGante <ferdegante.22@gmail.com>
2024-11-25 20:29:23 -0600

    scripts y devDependecies

--- 1-node-npm/1.4-api-express-npm/index.js
+++ 1-node-npm/1.4-api-express-npm/index.js
@@ -1,1 +1,24 @@
-// json canciones
+const express = require ('express');
+const colors = require('colors');
+const app = express();
+const port = 3000;
+
+// songs
+const songs = [
+    { id: 1, name: 'Kilometros', artist: 'Caligaris', year: 2007 },
+    { id: 2, name: 'Umbrella', artist: 'Rihanna', year: 2000 },
+    { id: 3, name: 'Back in black', artist: 'AC/DC', year: 1980 },
+    { id: 4, name: 'Gasolina', artist: 'Daddy Yankee', year: 2002 },
+];
+
+app.get('/', (req, res) => {
+    res.send('<html> <body> Hola desde node y express cambios </body> </html> ');
+    });
+
+app.get('/songs', (req, res) => {
+    res.json(songs);
+});
+
+app.listen(port, () => {
+    console.log(`Servidor JOYA en el puerto:${port}`.rainbow);
+}   );
